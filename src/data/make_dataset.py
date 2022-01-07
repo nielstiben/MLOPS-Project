8660e9a619172e7b357db473ec53d806146457ad
jaschn <49870201+jaschn@users.noreply.github.com>
2022-01-07 13:48:47 +0100

    PEP8 and typing update

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,45 +1,50 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 import os
 import zipfile
-import pandas
+from pathlib import Path
+
+import click
+from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
-@click.argument('dataset_path', type=click.Path())
-def main(dataset_path):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+@click.argument("dataset_path", type=click.Path())
+def main(dataset_path: str) -> None:
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('Downloading dataset from kaggle')
+    logger.info("Downloading dataset from kaggle")
     zip_folder = os.path.join(dataset_path, "raw")
 
     try:
         import kaggle
-    except:
-        logger.warning(f"Must athenticate the kaggle api according to https://www.kaggle.com/docs/api")
+    except Exception:
+        logger.warning(
+            "Must athenticate the kaggle api according to https://www.kaggle.com/docs/api"  # noqa: E501
+        )
         exit(1)
 
     try:
-        kaggle.api.competition_download_files("nlp-getting-started", path=zip_folder)
+        kaggle.api.competition_download_files("nlp-getting-started",
+                                              path=zip_folder)
     except Exception:
-        logger.warning(f"Must join the challange at: https://www.kaggle.com/c/nlp-getting-started/data")
+        logger.warning(
+            "Must join the challange at: https://www.kaggle.com/c/nlp-getting-started/data"  # noqa: E501
+        )
         exit(1)
 
     out_folder_raw = os.path.join(dataset_path, "interim")
     os.makedirs(out_folder_raw, exist_ok=True)
-    with zipfile.ZipFile(os.path.join(zip_folder, "nlp-getting-started.zip"), 'r') as zip_ref:
+    with zipfile.ZipFile(
+        os.path.join(zip_folder, "nlp-getting-started.zip"), "r"
+    ) as zip_ref:
         zip_ref.extractall(out_folder_raw)
 
-    
 
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
