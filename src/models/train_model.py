bf5b19a54c9eb73d639425c0e2176dcc0d7e5a00
kristinanett <kristinanett@gmail.com>
2022-01-13 11:10:20 +0100

    Added some workflow files

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -10,7 +10,6 @@ from pytorch_lightning import Trainer
 from src.data.dataset import DesasterTweetDataModule
 from src.models.model import MegaCoolTransformer
 
-
 @hydra.main(config_path="../../config", config_name="default_config.yaml")
 def main(config: DictConfig):
     logger = logging.getLogger(__name__)
