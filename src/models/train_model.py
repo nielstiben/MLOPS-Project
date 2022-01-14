564136f0cd12839e92cabda321f6218a2c1964ba
Wybren Oppedijk <s203130@student.dtu.dk>
2022-01-14 16:36:33 +0100

    wandb

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,12 +3,13 @@ import os
 from pathlib import Path
 
 import hydra
+import pytorch_lightning as pl
 import torch
+import wandb
 from dotenv import find_dotenv, load_dotenv
 from google.cloud import secretmanager
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import WandbLogger
 
 from src.data.dataset import DesasterTweetDataModule
 from src.models.model import MegaCoolTransformer
@@ -19,13 +20,14 @@ def main(config: DictConfig):
     logger = logging.getLogger(__name__)
     logger.info("Start Training...")
     client = secretmanager.SecretManagerServiceClient()
+    PROJECT_ID = "dtu-mlops-project"
 
-    response = client.access_secret_version(request={"name": "WANDB"})
-    os.environ["WANDB_API_KEY"] = response.payload.data.decode("utf-8")
-
-    wandb_logger = WandbLogger(
-        project="test-project",
-    )
+    secret_id = "WANDB"
+    resource_name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/latest"
+    response = client.access_secret_version(name=resource_name)
+    api_key = response.payload.data.decode("UTF-8")
+    os.environ["WANDB_API_KEY"] = api_key
+    wandb.init(project="dtu-mlops-project", config=config)
 
     gpus = 0
     if torch.cuda.is_available():
@@ -41,7 +43,11 @@ def main(config: DictConfig):
     )
     model = MegaCoolTransformer(config)
 
-    trainer = Trainer(max_epochs=2, gpus=gpus, logger=wandb_logger)
+    trainer = Trainer(
+        max_epochs=5,
+        gpus=gpus,
+        logger=pl.loggers.WandbLogger(project="mlops-mnist", config=config),
+    )
     trainer.fit(model, data_module)
     trainer.test(model, data_module)
 
