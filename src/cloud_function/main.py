f452d772a1b181b84764eeb51a8668b6cb8b2de7
jaschn <49870201+jaschn@users.noreply.github.com>
2022-01-20 11:49:22 +0100

    Updated cloud functions to clean tweet and return json

--- src/cloud_function/main.py
+++ src/cloud_function/main.py
@@ -1,8 +1,10 @@
 import io
 
+import nltk
 import torch
 from google.cloud import storage
 from transformers import AutoTokenizer
+from tweet_cleaner import clean_tweet
 
 BUCKET_NAME = "cloud_function_models"
 MODEL_FILE = "deployable_model.pt"
@@ -17,6 +19,9 @@ model = torch.jit.load(my_model)
 model.eval()
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
+nltk.download("wordnet")
+nltk.download("omw-1.4")
+
 
 def encode(text: str) -> list[int]:
     tokens = tokenizer.encode(text)
@@ -29,6 +34,7 @@ def encode(text: str) -> list[int]:
 def predict(request):
     request_json = request.get_json()
     tweet = request_json["tweet"]
+    tweet = clean_tweet(tweet)
     tweet = encode(tweet)
     tweet = torch.IntTensor(tweet)
     tweet.unsqueeze_(0)
@@ -36,11 +42,16 @@ def predict(request):
     pred = torch.argmax(pred, 1).item()
     if pred == 0:
         answer = "This is not a desaster tweet"
+        meme = "https://pyxis.nymag.com/v1/imgs/9ef/336/775d89db9c8ffcd8589f3acdf37d0e323f-25-this-is-fine-lede-new.2x.rhorizontal.w700.jpg"  # noqa: E501
     elif pred == 1:
         answer = "This is a desaster tweet"
+        meme = "https://wwwcache.wral.com/asset/news/local/2021/02/12/19524364/viral-raleigh-snow-glenwood-meme-DMID1-5putzq7om-640x360.jpg"  # noqa: E501
     else:
         answer = "error"
-    return answer
+        meme = "error"
+
+    resp = {"prediction": pred, "answer": answer, "picutre": meme}
+    return resp
 
 
 print("init done")
